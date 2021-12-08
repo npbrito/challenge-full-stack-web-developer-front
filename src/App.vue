e8993bcfedbe1ba0dccd8e82c60aa12ac005d4b9
npbrito <npazbrito@gmail.com>
2021-12-07 23:42:46 -0300

    Header implemented

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Headers/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ src/components/Headers/Header.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <b-navbar toggleable="sm" type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item href="/">Home</b-nav-item>
+      <b-nav-item href="/users">Usuários</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
